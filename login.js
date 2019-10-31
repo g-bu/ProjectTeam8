f694b13da9c4fd9cc8a04067513243abcccb66f8
Jeffry Lam Tang <jeffrylam_96@hotmail.ca>
2019-10-31 10:02:38 -0700

    login

---
+++ login.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDg-FN6Xg9nEtOtSFVN4ED-TlxL85kcmzc",
+    authDomain: "class-shortcut-tool.firebaseapp.com",
+    databaseURL: "https://class-shortcut-tool.firebaseio.com",
+    projectId: "class-shortcut-tool",
+    storageBucket: "class-shortcut-tool.appspot.com",
+    messagingSenderId: "580475693421",
+    appId: "1:580475693421:web:633c3316a68bbfbc1dd264"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
