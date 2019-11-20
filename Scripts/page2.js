586dd33f71c5a7d1fb6e3920024f13a1ffa702f3
g-bu <gracefbu@gmail.com>
2019-11-20 13:35:08 -0800

    page2's location B text field can now write to the firebase and read

--- Scripts/page2.js
+++ Scripts/page2.js
@@ -1,13 +1,38 @@
-function addRecent() {
-    firebase.auth().onAuthStateChanged(function(Users) {
-        db.collection('Users').doc(Users.uid)
-        .collection("recents").doc("recents")
-          .set(
-            {
-              "recent1": Users.displayRecent,
-              "recent2": Users.displayRecent,
-              "recent3": Users.displayRecent,
-            },
-            { merge: true });
-      });
+// function addRecent() {
+//     firebase.auth().onAuthStateChanged(function(Users) {
+//         db.collection('Users').doc(Users.uid)
+//         .collection("recents").doc("recents")
+//           .set(
+//             {
+//               "recent1": Users.displayRecent,
+//               "recent2": Users.displayRecent,
+//               "recent3": Users.displayRecent,
+//             },
+//             { merge: true });
+//       });
+// }
+
+document.getElementById('locationB').addEventListener('keyup', addRecent);
+
+function addRecent(e) {
+  if (e.keyCode === 13) {
+    console.log("test");
+    e.preventDefault();
+
+    //only referring to the user:'YI7AbuNz30GooRtldHJZ' to change value in recents
+    var dbref = db.collection('Users').doc('YI7AbuNz30GooRtldHJZ').collection('recents').doc('recents');
+    var locB = document.getElementById('locationB').value;
+    console.log(locB);
+  
+    dbref.set({
+      "recent1": locB,
+    });
+  }
+}
+
+function myFunction() {
+  var x = document.getElementById("locationB").value;
+  document.getElementById("demo").innerHTML = "You wrote: " + x;
+
+  //console.log(x);
 }
