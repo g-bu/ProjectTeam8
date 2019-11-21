50e85235392f714f0ba07d6f6e86c1b99fdbd882
Rob Wilson <distracted.abstraction@gmail.com>
2019-11-21 10:16:28 -0800

    morning changes nov 21

--- Scripts/page2.js
+++ Scripts/page2.js
@@ -5,16 +5,20 @@ document.getElementById('locationB').addEventListener('keyup', addRecent);
 
 function addRecent(e) {
   if (e.keyCode === 13) {
-    console.log("test recent1");
+    console.log('test recent1');
     e.preventDefault();
 
     //only referring to the user:'YI7AbuNz30GooRtldHJZ' to change value in recents
-    var dbref = db.collection('Users').doc('YI7AbuNz30GooRtldHJZ').collection('recents').doc('recent1');
+    var dbref = db
+      .collection('Users')
+      .doc('YI7AbuNz30GooRtldHJZ')
+      .collection('recents')
+      .doc('recent1');
     var locB = document.getElementById('locationB').value;
     console.log(locB);
 
     dbref.set({
-      "recent1": locB,
+      name: locB
     });
   }
 }
@@ -47,18 +51,21 @@ function addRecent(e) {
 //         }
 //       }
 
-    //---------------------------------------------------
-    // Reads User's Recent Locations from database and 
-    //      inputs values into the containers for each
-    //---------------------------------------------------
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-    const db = firebase.firestore();
-    //only referring to the user:'YI7AbuNz30GooRtldHJZ' to change value in recents
-    db.collection('Users').doc('YI7AbuNz30GooRtldHJZ').collection('recents').doc('recent1')
-      .onSnapshot(function (snap) {
-        console.log('this is working test...', snap.data());
-        document.getElementById('recent1').innerHTML = snap.data().recent1; // another way is to go snap.data()['recent1']
-        document.getElementById('recent2').innerHTML = snap.data().recent2;
-        document.getElementById('recent3').innerHTML = snap.data().recent3;
-      });
+//---------------------------------------------------
+// Reads User's Recent Locations from database and
+//      inputs values into the containers for each
+//---------------------------------------------------
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+//only referring to the user:'YI7AbuNz30GooRtldHJZ' to change value in recents
+db.collection('Users')
+  .doc('YI7AbuNz30GooRtldHJZ')
+  .collection('recents')
+  .doc('recent1')
+  .onSnapshot(function(snap) {
+    console.log('this is working test...', snap.data());
+    document.getElementById('recent1').innerHTML = snap.data().name; // another way is to go snap.data()['recent1']
+    document.getElementById('recent2').innerHTML = snap.data().recent2;
+    document.getElementById('recent3').innerHTML = snap.data().recent3;
+  });
