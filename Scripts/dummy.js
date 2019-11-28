0cb1228fd5be575aeb5fe1eb33fb6e72a5269989
Jeffry Lam Tang <jeffrylam_96@hotmail.ca>
2019-11-28 10:42:19 -0800

    clear function and fixing firebase problems

--- Scripts/dummy.js
+++ Scripts/dummy.js
@@ -10,3 +10,18 @@ function searchPage(){
   var page2 = "page2.html";
   window.location = page2;
 }
+$('#sw5').click(function(event) {
+  console.log('s45 was clicked, yeah it worked');
+  $('#htmlForSW5').css('display', 'grid');
+});
+
+$('#htmlForSW5').hover(function(event) {
+  console.log('dbl click happened was clicked, yeah it worked');
+  $('#htmlForSW5').css('display', 'none');
+});
+
+function clearPage(){
+  $('#htmlForSW5').css('display', 'none');
+  $('#htmlForSE12').css('display', 'none');
+  $('#htmlForSE14').css('display', 'none');
+}
