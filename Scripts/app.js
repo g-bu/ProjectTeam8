98b31bab6fa717868fa7eff281f247610da8bee9
Robert Wilson <robertwilson875@gmail.com>
2019-12-04 20:11:26 -0800

    deleted app.js

--- Scripts/app.js
+++
@@ -1,6 +0,0 @@
-function showName() {
-  firebase.auth().onAuthStateChanged(function(Users) {
-    console.log(Users);
-    document.getElementById('username').innerHTML = Users.displayName;
-  });
-}
